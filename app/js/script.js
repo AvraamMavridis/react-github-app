1e5f9beffdecd6a1ebdade8f9267cd3ae0ad47f6
AvraamMavridis <avr.mav@gmail.com>
2015-07-28 08:33:33 +0300

    (FEAT) use Susy, setup gulp sass workflow

---
+++ app/js/script.js
@@ -0,0 +1,33 @@
+var myApp = {};
+
+myApp.Something = React.createClass({
+
+  getDefaultProps: function(){
+    return {
+      username: '',
+      repository: ''
+    }
+  },
+
+  setUserName: function(event){
+    this.setState({
+      username: event.target.value
+    });
+  },
+
+  setRepository: function(event){
+   this.setState({ repository: event.target.value })
+  },
+
+  render: function(){
+    console.log(this.state)
+    return(
+      <span>
+        <input type="text" name="username" onChange={this.setUserName}/>
+        <input type="text" name="repository" onChange={this.setRepository}/>
+      </span>
+    );
+  }
+});
+
+React.render(<myApp.Something/>, document.getElementById('content'))
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp        = require("gulp");
+//var concat      = require("gulp-concat");
+var connect     = require("gulp-connect");
+var sass        = require('gulp-ruby-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+var mergeStream = require('merge-stream');
+
+gulp.task("connect", function(){
+  connect.server({
+    root: 'build/',
+    port: 8000
+  })
+});
+
+gulp.task('sass', function(){
+  options = {
+    compass: true,
+    force: true,
+    sourcemap: true
+  }
+
+  sass('app/main.sass', options)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/'))
+});
+
+
+gulp.task('copy', function(){
+
+  js = gulp.src('app/js/**/*', { base: 'app/' })
+           .pipe(gulp.dest('build/'))
+
+  html = gulp.src('app/**/*.html', { base: 'app/' })
+             .pipe(gulp.dest('build/'))
+
+  mergeStream(js,html)
+});
